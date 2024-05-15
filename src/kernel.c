47b278ea719ade901c4f9b8eb7c26d4c5711e054
Andrii0302 <hfdkwgdbr@gmail.com>
2024-05-15 20:22:45 +0300

    elf loader

--- src/kernel.c
+++ src/kernel.c
@@ -148,9 +148,9 @@ void kernel_main()
     keyboard_init();
     // idt_register_interrupt_callback(0x20, pic_timer_callback);
     struct process* process = 0;
-    int res = process_load_switch("0:/blank.bin", &process);
+    int res = process_load_switch("0:/blank.elf", &process);
     if (res != NEWOS_ALL_OK){
-        panic("Failed to load blank.bin\n");
+        panic("Failed to load blank.elf\n");
     }
     //keyboard_push('A');
 
--- src/status.h
+++ src/status.h
@@ -10,4 +10,5 @@
 #define ERDONLY 6
 #define EUNIMP 7
 #define EISTKN 8
+#define EINFORMAT 9
 #endif
